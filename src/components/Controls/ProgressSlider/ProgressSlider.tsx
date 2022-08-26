136492f6e0bf7016c1129ea9f1423dc49f16955c
hoangvu12 <hggaming91@gmail.com>
2022-08-26 11:28:00 +0700

    fix: 🐛 move thumbnail hover to another component

--- src/components/Controls/ProgressSlider/ProgressSlider.tsx
+++ src/components/Controls/ProgressSlider/ProgressSlider.tsx
@@ -1,55 +1,16 @@
-import { parse } from '@plussub/srt-vtt-parser';
-import { Entry } from '@plussub/srt-vtt-parser/dist/src/types';
-import React, {
-  useCallback,
-  useEffect,
-  useLayoutEffect,
-  useMemo,
-  useState,
-} from 'react';
-import { buildAbsoluteURL } from 'url-toolkit';
-import { PLAYER_CONTAINER_CLASS } from '../../../constants';
-import { useVideoProps } from '../../../contexts';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
 import { useVideo } from '../../../contexts/VideoContext';
-import { usePopover } from '../../../hooks';
-import { classNames, convertTime, isValidUrl } from '../../../utils';
+import { classNames, convertTime } from '../../../utils';
 import { isDesktop } from '../../../utils/device';
-import Portal from '../../Portal';
 import Slider from '../../Slider';
+import ThumbnailHover from '../ThumbnailHover';
 import styles from './ProgressSlider.module.css';
-
-const playerContainerClass = '.' + PLAYER_CONTAINER_CLASS;
 
 const ProgressSlider = () => {
   const { videoEl, setVideoState } = useVideo();
-  const { thumbnail } = useVideoProps();
-  const [thumbnailEntries, setThumbnailEntries] = useState<Entry[]>([]);
   const [bufferPercent, setBufferPercent] = useState(0);
   const [hoverPercent, setHoverPercent] = useState(0);
   const [currentTime, setCurrentTime] = useState(0);
-  const [portalElement, setPortalElement] = useState<HTMLDivElement | null>(
-    null
-  );
-
-  const { floatingRef, referenceRef, update, strategy, x, y } = usePopover<
-    HTMLDivElement,
-    HTMLDivElement
-  >({
-    offset: 10,
-    strategy: 'fixed',
-    overflowElement: playerContainerClass,
-    position: 'top',
-  });
-
-  useLayoutEffect(() => {
-    const el = document.querySelector(playerContainerClass) as HTMLDivElement;
-
-    if (!el) return;
-
-    setPortalElement(el);
-
-    update();
-  }, [update]);
 
   // https://stackoverflow.com/questions/5029519/html5-video-percentage-loaded
   useEffect(() => {
@@ -132,62 +93,6 @@ const ProgressSlider = () => {
     [setVideoState, videoEl]
   );
 
-  useEffect(() => {
-    if (!thumbnail) return;
-    if (!videoEl) return;
-
-    const fetchThumbnails = async () => {
-      const response = await fetch(thumbnail);
-
-      const text = await response.text();
-
-      const { entries = [] } = parse(text);
-
-      setThumbnailEntries(entries);
-    };
-
-    fetchThumbnails();
-  }, [thumbnail, videoEl]);
-
-  const currentThumbnail = useMemo(() => {
-    if (!thumbnailEntries?.length) return;
-    if (!videoEl?.duration) return;
-
-    const currentTime = (hoverPercent / 100) * videoEl.duration * 1000;
-
-    const currentEntry = thumbnailEntries.find(
-      (entry) => entry.from <= currentTime && entry.to > currentTime
-    );
-
-    if (!currentEntry?.text) return undefined;
-
-    const thumbnailUrlRaw = isValidUrl(currentEntry.text)
-      ? currentEntry.text
-      : buildAbsoluteURL(thumbnail, currentEntry.text);
-
-    const { origin, pathname } = new URL(thumbnailUrlRaw);
-
-    const thumbnailUrl = origin + pathname;
-
-    const [x, y, w, h] = thumbnailUrlRaw
-      ?.split('=')[1]
-      .split(',')
-      .map((a) => a.trim());
-
-    // Update thumbnail position
-    update();
-
-    return {
-      rect: {
-        x: -1 * Number(x),
-        y: -1 * Number(y),
-        w: Number(w),
-        h: Number(h),
-      },
-      url: thumbnailUrl,
-    };
-  }, [hoverPercent, thumbnail, thumbnailEntries, update, videoEl?.duration]);
-
   return (
     <Slider
       className={classNames(styles.container, isDesktop && styles.desktop)}
@@ -204,34 +109,7 @@ const ProgressSlider = () => {
         <Slider.Bar className={styles.backgroundBar} />
         <Slider.Dot className={styles.dot} percent={currentPercent} />
 
-        {currentThumbnail && portalElement && (
-          <React.Fragment>
-            <div
-              ref={referenceRef}
-              className={styles.hoverThumbnailReference}
-              style={{ left: hoverPercent + '%' }}
-            />
-
-            <Portal element={portalElement}>
-              <div
-                className={styles.hoverThumbnail}
-                ref={floatingRef}
-                style={{
-                  top: y + 'px',
-                  left: x + 'px',
-                  position: strategy,
-                  display: hoverPercent > 0 ? 'block' : 'none',
-                  width: currentThumbnail.rect.w,
-                  height: currentThumbnail.rect.h,
-                  backgroundImage: `url(${currentThumbnail.url})`,
-                  backgroundPositionX: currentThumbnail.rect.x,
-                  backgroundPositionY: currentThumbnail.rect.y,
-                  backgroundRepeat: 'no-repeat',
-                }}
-              />
-            </Portal>
-          </React.Fragment>
-        )}
+        <ThumbnailHover hoverPercent={hoverPercent} />
 
         {!!hoverPercent && videoEl?.duration && (
           <div
---
+++ src/components/Controls/ThumbnailHover.tsx
@@ -0,0 +1,132 @@
+import { parse } from '@plussub/srt-vtt-parser';
+import { Entry } from '@plussub/srt-vtt-parser/dist/src/types';
+import React, { useEffect, useLayoutEffect, useMemo, useState } from 'react';
+import { buildAbsoluteURL } from 'url-toolkit';
+import { PLAYER_CONTAINER_CLASS } from '../../constants';
+import { useVideo, useVideoProps } from '../../contexts';
+import { usePopover } from '../../hooks';
+import { isValidUrl } from '../../utils';
+import Portal from '../Portal';
+import styles from './ProgressSlider/ProgressSlider.module.css';
+
+const playerContainerClass = '.' + PLAYER_CONTAINER_CLASS;
+
+interface ThumbnailHoverProps {
+  hoverPercent: number;
+}
+
+const ThumbnailHover: React.FC<ThumbnailHoverProps> = ({ hoverPercent }) => {
+  const [portalElement, setPortalElement] = useState<HTMLDivElement | null>(
+    null
+  );
+  const [thumbnailEntries, setThumbnailEntries] = useState<Entry[]>([]);
+  const { thumbnail } = useVideoProps();
+  const { videoEl } = useVideo();
+
+  const { floatingRef, referenceRef, update, strategy, x, y } = usePopover<
+    HTMLDivElement,
+    HTMLDivElement
+  >({
+    offset: 10,
+    strategy: 'fixed',
+    overflowElement: playerContainerClass,
+    position: 'top',
+  });
+
+  useLayoutEffect(() => {
+    const el = document.querySelector(playerContainerClass) as HTMLDivElement;
+
+    if (!el) return;
+
+    setPortalElement(el);
+
+    update();
+  }, [update]);
+
+  useEffect(() => {
+    if (!thumbnail) return;
+    if (!videoEl) return;
+
+    const fetchThumbnails = async () => {
+      const response = await fetch(thumbnail);
+
+      const text = await response.text();
+
+      const { entries = [] } = parse(text);
+
+      setThumbnailEntries(entries);
+    };
+
+    fetchThumbnails();
+  }, [thumbnail, videoEl]);
+
+  const currentThumbnail = useMemo(() => {
+    if (!thumbnailEntries?.length) return;
+    if (!videoEl?.duration) return;
+
+    const currentTime = (hoverPercent / 100) * videoEl.duration * 1000;
+
+    const currentEntry = thumbnailEntries.find(
+      (entry) => entry.from <= currentTime && entry.to > currentTime
+    );
+
+    if (!currentEntry?.text) return undefined;
+
+    const thumbnailUrlRaw = isValidUrl(currentEntry.text)
+      ? currentEntry.text
+      : buildAbsoluteURL(thumbnail, currentEntry.text);
+
+    const { origin, pathname } = new URL(thumbnailUrlRaw);
+
+    const thumbnailUrl = origin + pathname;
+
+    const [x, y, w, h] = thumbnailUrlRaw
+      ?.split('=')[1]
+      .split(',')
+      .map((a) => a.trim());
+
+    // Update thumbnail position
+    update();
+
+    return {
+      rect: {
+        x: -1 * Number(x),
+        y: -1 * Number(y),
+        w: Number(w),
+        h: Number(h),
+      },
+      url: thumbnailUrl,
+    };
+  }, [hoverPercent, thumbnail, thumbnailEntries, update, videoEl?.duration]);
+
+  return currentThumbnail && portalElement ? (
+    <React.Fragment>
+      <div
+        ref={referenceRef}
+        className={styles.hoverThumbnailReference}
+        style={{ left: hoverPercent + '%' }}
+      />
+
+      <Portal element={portalElement}>
+        <div
+          className={styles.hoverThumbnail}
+          ref={floatingRef}
+          style={{
+            top: y + 'px',
+            left: x + 'px',
+            position: strategy,
+            display: hoverPercent > 0 ? 'block' : 'none',
+            width: currentThumbnail.rect.w,
+            height: currentThumbnail.rect.h,
+            backgroundImage: `url(${currentThumbnail.url})`,
+            backgroundPositionX: currentThumbnail.rect.x,
+            backgroundPositionY: currentThumbnail.rect.y,
+            backgroundRepeat: 'no-repeat',
+          }}
+        />
+      </Portal>
+    </React.Fragment>
+  ) : null;
+};
+
+export default ThumbnailHover;
